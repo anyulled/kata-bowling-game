4843ab257915119cabefea0356790c24058eebac
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2023-12-13 23:10:11 +0100

    feat: scenario configuration

---
+++ src/main/java/com/ingram/BowlingGame.java
@@ -0,0 +1,13 @@
+package com.ingram;
+
+public class BowlingGame {
+
+    public void roll(int pins){
+        // business logic here
+    }
+
+    public int score(){
+        // business logic here
+        return -1;
+    }
+}
---
+++ src/test/java/BowlingGameTest.java
@@ -0,0 +1,10 @@
+import com.ingram.BowlingGame;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BowlingGameShould {
+    BowlingGame game;
+
+}
